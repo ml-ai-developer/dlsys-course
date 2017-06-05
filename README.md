3ab0ce4defc540796d42b96ab0e676e9de4e50b2
ml-ai-developer <ml.ai.developer@gmail.com>
2017-06-05 23:25:03 +0800

    Assignment 1 Finished!!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dlsys-course
 CS 599G1: Deep Learning System
+
+Since I don't have a GPU now, assignement2 is to be continued.
--- assignment1/autodiff_test.py
+++ assignment1/autodiff_test.py
@@ -190,7 +190,7 @@ def test_matmul_two_vars():
     x2_val = np.array([[1, 2], [3, 4], [5, 6]])  # 3x2
     x3_val = np.array([[7, 8, 9], [10, 11, 12]])  # 2x3
 
-    y_val, grad_x2_val, grad_x3_val = executor.run(feed_dict={x2: x2_val, x3: x3_val})
+    y_val, grad_x2_val, grad_x3_val = executor.run(feed_dict={assignment2x2: x2_val, x3: x3_val})
 
     expected_yval = np.matmul(x2_val, x3_val)
     expected_grad_x2_val = np.matmul(np.ones_like(expected_yval), np.transpose(x3_val))
